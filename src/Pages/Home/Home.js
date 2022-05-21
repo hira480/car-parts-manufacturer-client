362315da370492cfe5af7bff5c507512e2fed569
hira480 <hiramannan8@gmail.com>
2022-05-22 02:41:45 +0600

    navbar added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BusinessSummary from './BusinessSummary';
+
+const Home = () => {
+    return (
+        <div>
+            <BusinessSummary></BusinessSummary>
+        </div>
+    );
+};
+
+export default Home;
