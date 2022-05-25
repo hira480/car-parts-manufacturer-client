258a7bd4e83a70c5511bd2544d516212d6e9e2c3
hira480 <hiramannan8@gmail.com>
2022-05-26 02:34:15 +0600

    basic us design for payment

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L1DwyJAH8q8uEVYs1gJMDYrFovUV3aBm6L9C0drCV12ILnYk8MMrpWWuZaSTLf9koM1xQAFngWiQ3mgEs8B7BCl00Zhc8MRxP');
 
 const Payment = () => {
     const { id } = useParams();
@@ -33,7 +38,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-lg shadow-2xl bg-base-100">
                 <div class="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
 
                 </div>
             </div>
